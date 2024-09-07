9afc6d021e23cbf9b80e8eca3a83adb06e2ec7c5
KALEEM IQBAL <olduser0071@gmail.com>
2024-09-07 07:35:44 +0530

    break the pages

---
+++ src/pages/AsyncCounter.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../app/store";
+
+import {
+  simulateAsyncIncrement,
+  simulateAsyncDecrement,
+} from "../redux/actions/counterActions";
+
+const AsyncCounter: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const { status } = useSelector((state: RootState) => state.counter);
+  const [incrementAmount, setIncrementAmount] = useState<number>(0);
+  const [decrementAmount, setDecrementAmount] = useState<number>(0);
+
+  const handleAsyncIncrement = () => {
+    dispatch(simulateAsyncIncrement(incrementAmount));
+  };
+
+  const handleAsyncDecrement = () => {
+    dispatch(simulateAsyncDecrement(decrementAmount));
+  };
+  return (
+    <>
+      <div className="row mb-4">
+        <div className="col-8 mb-3">
+          <input
+            type="number"
+            className="form-control"
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(Number(e.target.value))}
+            placeholder="Amount to increment"
+          />
+        </div>
+        <div className="col-4 d-flex justify-content-center">
+          <button className="btn btn-success" onClick={handleAsyncIncrement}>
+            Async Increment
+          </button>
+        </div>
+      </div>
+      <div className="row mb-4">
+        <div className="col-8 mb-3">
+          <input
+            type="number"
+            className="form-control"
+            value={decrementAmount}
+            onChange={(e) => setDecrementAmount(Number(e.target.value))}
+            placeholder="Amount to decrement"
+          />
+        </div>
+        <div className="col-4 d-flex justify-content-center">
+          <button
+            className="btn btn-warning btn-md"
+            onClick={handleAsyncDecrement}
+          >
+            Async Decrement
+          </button>
+        </div>
+      </div>
+      {status === "loading" && (
+        <p className="text-info text-center">Loading...</p>
+      )}
+      {status === "failed" && (
+        <p className="text-danger text-center">Failed to update counter</p>
+      )}
+    </>
+  );
+};
+
+export default AsyncCounter;
--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../app/store";
 import { increment, decrement } from "../redux/reducers/counterReducer";
-import {
-  simulateAsyncIncrement,
-  simulateAsyncDecrement,
-} from "../redux/actions/counterActions";
+
+import AsyncCounter from "./AsyncCounter";
 
 const CounterPage: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
-  const { value, status } = useSelector((state: RootState) => state.counter);
-  const [incrementAmount, setIncrementAmount] = useState<number>(0);
-  const [decrementAmount, setDecrementAmount] = useState<number>(0);
+  const { value } = useSelector((state: RootState) => state.counter);
 
   const handleIncrement = () => {
     dispatch(increment());
@@ -21,18 +17,10 @@ const CounterPage: React.FC = () => {
     dispatch(decrement());
   };
 
-  const handleAsyncIncrement = () => {
-    dispatch(simulateAsyncIncrement(incrementAmount));
-  };
-
-  const handleAsyncDecrement = () => {
-    dispatch(simulateAsyncDecrement(decrementAmount));
-  };
-
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
-        <div className="col-md-6">
+        <div className="col">
           <div className="card shadow-sm">
             <div className="card-header bg-primary text-white">
               <h5 className="mb-0">Counter</h5>
@@ -57,52 +45,7 @@ const CounterPage: React.FC = () => {
                   </button>
                 </div>
               </div>
-              <div className="row mb-4">
-                <div className="col-12 mb-3">
-                  <input
-                    type="number"
-                    className="form-control"
-                    value={incrementAmount}
-                    onChange={(e) => setIncrementAmount(Number(e.target.value))}
-                    placeholder="Amount to increment"
-                  />
-                </div>
-                <div className="col-12 d-flex justify-content-center">
-                  <button
-                    className="btn btn-success"
-                    onClick={handleAsyncIncrement}
-                  >
-                    Async Increment
-                  </button>
-                </div>
-              </div>
-              <div className="row mb-4">
-                <div className="col-12 mb-3">
-                  <input
-                    type="number"
-                    className="form-control"
-                    value={decrementAmount}
-                    onChange={(e) => setDecrementAmount(Number(e.target.value))}
-                    placeholder="Amount to decrement"
-                  />
-                </div>
-                <div className="col-12 d-flex justify-content-center">
-                  <button
-                    className="btn btn-warning"
-                    onClick={handleAsyncDecrement}
-                  >
-                    Async Decrement
-                  </button>
-                </div>
-              </div>
-              {status === "loading" && (
-                <p className="text-info text-center">Loading...</p>
-              )}
-              {status === "failed" && (
-                <p className="text-danger text-center">
-                  Failed to update counter
-                </p>
-              )}
+              <AsyncCounter />
             </div>
           </div>
         </div>
